256ef1ce665e273e3be34c7beef97656816121b6
GABRIEL OLIVEIRA DAMASCENO <gabriel.odamasceno@senacsp.edu.br>
2022-06-20 21:32:09 -0300

    Aula 20/06/2022 (Completo) ver1.0

---
+++ p11-api-bycrypt/app.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const app = express();
+const port = 4500;
+const User = require('./database/models/User');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.send("Serviço da API Rest iniciada...");
+});
+
+app.get("/users", async (req, res) => {
+    await User.findAll({
+        attributes: ['id', 'name', 'email', 'gender'],
+        order:[['name', 'ASC']]
+    }).then((users) => {
+        return res.json({
+            erro: false,
+            users
+        });
+    }).catch((err) => {
+        return res.status(400).json({
+            erro: true,
+            mensagem: `Erro: ${err} ou Nenhum Usuário Encontrado`
+        });
+    });
+});
+
+app.get('/users/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const users = await User.findByPk(id);
+        if(!users){
+            return res.status(400).json({
+                erro: true,
+                mensagem: "Erro: Nenhum Usuário Encontrado"
+            });
+        };
+        res.status(200).json({
+            erro: false,
+            users
+        });
+    }catch(err){
+        res.status(400).json({
+            erro: true,
+            mensagem: `Erro: ${err}`
+        });
+    };
+});
+
+app.post("/user", async (req, res) => {
+    // const {name, email, gender, password} = req.body;
+    await User.create(req.body)
+    .then(() => {
+        return res.json({
+            erro: false,
+            mensagem: 'Usuário Cadastrado com Sucesso !'
+        });
+    }).catch((err) => {
+        return res.status(400).json({
+            erro: true,
+            mensagem: `Erro: Usuário não Cadastrado ${err}`
+        });
+    });
+});
+
+app.put("/user", async (req, res) => {
+    const {id} = req.body;
+
+    await User.update(req.body, {where: {id}})
+    .then(() => {
+        return res.json({
+            erro: false,
+            mensagem: 'Usuário Alterado com Sucesso !'
+        });
+    }).catch((err) => {
+        return res.status(400).json({
+            erro: true,
+            mensagem: `Erro: Usuário não Alterado ${err}`
+        });
+    });
+});
+
+app.delete("/user/:id", async (req, res) => {
+    const {id} = req.params;
+    await User.destroy({where:{id}})
+    .then(() => {
+        return res.json({
+            erro: false,
+            mensagem: "Usuário Deletado com Sucesso !"
+        });
+    }).catch((err) => {
+        return res.status(400).json({
+            erro: true,
+            mensagem: `Erro: ${err}, Usuário não Deletado`
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor iniciado na porta: ${port}: "http://localhost:${port}"`);
+});
